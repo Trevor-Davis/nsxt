16cf5601669fe08810f0f3cd21819ff8c720b580
Trevor-Davis <56545070+Trevor-Davis@users.noreply.github.com>
2022-04-20 17:29:42 +0000

    updates

--- getroutetable.ps1
+++ getroutetable.ps1
@@ -5,58 +5,21 @@ if (Test-Path -Path $Folder) {
 mkdir $env:TEMP\routetable
 }
 
+$filename1 = "logintonsx.ps1"
+Invoke-WebRequest -uri "https://raw.githubusercontent.com/Trevor-Davis/nsxt/main/$filename1" `
+-OutFile $Folder\$filename1
 
-
-$filename = "logintonsx.ps1"
-Invoke-WebRequest -uri "https://raw.githubusercontent.com/Trevor-Davis/Azure-VMware-Solution/master/Nested-Lab-Deployment/$filename" `
--OutFile $Folder\$filename
-
-$filename = "createt1router.ps1"
-Invoke-WebRequest -uri "https://raw.githubusercontent.com/Trevor-Davis/Azure-VMware-Solution/master/Nested-Lab-Deployment/$filename" `
--OutFile $Folder\$filename
-
-$filename = "createswitchingprofiles.ps1"
-Invoke-WebRequest -uri "https://raw.githubusercontent.com/Trevor-Davis/Azure-VMware-Solution/master/Nested-Lab-Deployment/$filename" `
--OutFile $Folder\$filename
-
-$filename = "createswitches.ps1"
-Invoke-WebRequest -uri "https://raw.githubusercontent.com/Trevor-Davis/Azure-VMware-Solution/master/Nested-Lab-Deployment/$filename" `
--OutFile $Folder\$filename
-
-$filename = "createt1downlinkports.ps1"
-Invoke-WebRequest -uri "https://raw.githubusercontent.com/Trevor-Davis/Azure-VMware-Solution/master/Nested-Lab-Deployment/$filename" `
--OutFile $Folder\$filename
-
-$filename = "createt1downlinkports.ps1"
-Invoke-WebRequest -uri "https://raw.githubusercontent.com/Trevor-Davis/Azure-VMware-Solution/master/Nested-Lab-Deployment/$filename" `
--OutFile $Folder\$filename
-
-$filename = "createswitchuplinkports.ps1"
-Invoke-WebRequest -uri "https://raw.githubusercontent.com/Trevor-Davis/Azure-VMware-Solution/master/Nested-Lab-Deployment/$filename" `
--OutFile $Folder\$filename
-
-$filename = "linkswitchandt1.ps1"
-Invoke-WebRequest -uri "https://raw.githubusercontent.com/Trevor-Davis/Azure-VMware-Solution/master/Nested-Lab-Deployment/$filename" `
--OutFile $Folder\$filename
-
-$filename = "configuret1routeadvertisement.ps1"
-Invoke-WebRequest -uri "https://raw.githubusercontent.com/Trevor-Davis/Azure-VMware-Solution/master/Nested-Lab-Deployment/$filename" `
--OutFile $Folder\$filename
-
+$filename2 = "routetable.ps1"
+Invoke-WebRequest -uri "https://raw.githubusercontent.com/Trevor-Davis/nsxt/main/$filename2" `
+-OutFile $Folder\$filename2
 
 ####################
 #DELETE THIS
 #####################
-Copy-Item "C:\Users\avs-admin\Documents\GitHub\Azure-VMware-Solution\Nested-Lab-Deployment\*" -Destination "$Folder"
+Copy-Item "C:\Users\avs-admin\Documents\GitHub\nsxt\*" -Destination "$Folder"
 
+Clear-host
 
-
-Invoke-Expression -Command $Folder\variables.ps1
-Invoke-Expression -Command $Folder\createt1router.ps1
-Invoke-Expression -Command $Folder\createswitchingprofiles.ps1
-Invoke-Expression -Command $Folder\createswitches.ps1
-Invoke-Expression -Command $Folder\createt1downlinkports.ps1
-Invoke-Expression -Command $Folder\createswitchuplinkports.ps1
-Invoke-Expression -Command $Folder\createresourcepool.ps1
-Invoke-Expression -Command $Folder\linkswitchandt1.ps1
-Invoke-Expression -Command $Folder\configuret1routeadvertisement.ps1
+Invoke-Expression -Command $Folder\$filename1
+Clear-host
+Invoke-Expression -Command $Folder\$filename2
--- routetable.ps1
+++ routetable.ps1
@@ -1,4 +1,14 @@
 $body = ""
 
 $response = Invoke-RestMethod https://$nsxtip/policy/api/v1/infra/tier-0s/$t0routername/routing-table?format=csv -Method 'GET' -Headers $headers -Body $body -SkipCertificateCheck
-$response | Out-String | Set-Content c:\temp\routetable.csv
+$response | Out-String | Set-Content $Folder\routetable.csv
+Write-Host -ForegroundColor Green "You can find the routetable.csv file here $Folder" 
+Write-Host -ForegroundColor Yellow "
+Do you want to open the file? (Y/N): " -NoNewline
+$question = Read-Host
+
+if ($question -eq "y"){
+
+    Invoke-Expression -Command $Folder\routetable.csv
+
+}
